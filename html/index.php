1d083f25b0856af459fb7ad73e5583a4b13cc590
Tim McHugh <tim@digitaltim.com>
2019-01-23 11:37:20 -0500

    Updates compose file with some additional options and comments. Updates index file with boilerplate HTML

--- html/index.php
+++ html/index.php
@@ -1,3 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test Page</title>
+</head>
+<body>
+    <h1>Test Page</h1>
+    <?php
+        phpinfo();
+    ?>
 
-phpinfo();
+</body>
+</html>
