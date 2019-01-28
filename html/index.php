3ec40379b7ff723530e79bc69164a9cc8d33b739
Tim McHugh <tim@digitaltim.com>
2019-01-28 13:09:25 -0500

    Adds PostgreSQL PHP extension and tests for this and PDO connection status on index page

--- html/index.php
+++ html/index.php
@@ -15,6 +15,26 @@
             echo "<h1>The server is running. Try <a href='https://{$_SERVER['HTTP_HOST']}'>HTTPS</a></h1>";
             echo "<h1>The server is running. Try <a href='https://{$_SERVER['SERVER_NAME']}'>HTTPS</a></h1>";
         }
+        if ($dbconn = pg_connect("host=postgres11.1-container dbname=testdb user=postgres password=passdb")) {
+            echo "<h2>Connected to DB using pg_connect</h2>";
+            // print_r(pg_version($dbconn));
+        } else {
+            echo "<h2>Cannot connect to DB using pg_connect</h2>";
+        }
+        
+        $dsn = 'pgsql:dbname=testdb;host=postgres11.1-container';
+        $user = 'postgres';
+        $password = 'passdb';
+
+        try {
+            if($dbh = new PDO($dsn, $user, $password)) {
+                echo "<h2>Connected to DB using PDO</h2>";
+            } else {
+                echo "<h2>Cannot connect to DB using PDO</h2>";
+            }
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+        }
     ?>
     <?php
         phpinfo();
